d0fa239650045ea8cf7c30d9f9f448138d27e3dd
bahar77-mm <4fbaharff@gmail.com>
2026-08-09 16:37:46 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+[source: 4]import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Firebase Configuration
